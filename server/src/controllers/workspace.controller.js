bb2e90d3b705541a5fe7dfdd3b6014de0fad4ef7
WenukaKithnula <wkithnula@gmail.com>
2026-09-08 16:13:33 +0530

    created the getwork end point

--- server/src/controllers/workspace.controller.js
+++ server/src/controllers/workspace.controller.js
@@ -2,22 +2,37 @@ import pool from '../db/pool.js';
 
 export async function createWorkspace(req,res){
   const { authId } = req;
-  const{name} = req.body.name
+  const  name  = req.body.name;
 
   try{
-
     const result = await pool.query(
     `INSERT INTO workspaces (auth_id, name)
        VALUES ($1, $2)
        RETURNING *`,
     [authId ,name ]
-    
   );
-  return res.status(201).json({message:result.rows[0]})
-
-
+  return res.status(201).json({ workspace: result.rows[0] })
   }
   catch(err){
+    console.error(err);
     return res.status(500).json({ error: `Could not create ${name}` });
   }
 }
+
+export async function getworkspaces(req,res){
+
+  const{authId} = req;
+  try{
+
+    const results = await pool.query(
+      `SELECT * FROM workspaces WHERE auth_id = $1
+       `
+       [authId]
+    )
+    res.status(200).json(results.rows)
+  }
+  catch(err){
+    res.status(500).json({error:"could not fetch the workspaces"})
+  }
+}
+
